b691e13f847f7491ba61a24807ec0951dc744cd5
Miguel <mi.trds@pm.me>
2022-04-11 15:00:46 -0300

    * suporte a api/gerenciamento de erros

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,10 +5,20 @@ const baseUrl = "https://animeyabu.com/"
 /**
  * Faz uma requisição ao animeyabu.com
  * @param {string} path
+ * @param {boolean} api Se a requisição deve ser feita a API do site
  * @returns {Promise<string | null>}
  */
-export default async function request(path = "") {
-    const requestUrl = baseUrl + path
-    const response = await fetch(requestUrl)
-    return response.url !== requestUrl ? null : response.text()
+export default async function request(path = "", api) {
+    const url = `${baseUrl}${api ? "api/" : ""}${path}`;
+    try {
+        const responseStatus = await fetch(url);
+        if (responseStatus.redirected) {
+           return null;
+        }
+        const responseData = await (api ? response.json() : response.text)
+        return responseData
+    } catch(err) {
+        console.log(`Algo deu errado ao fazer a requisição: ${err} ${err.stack}`);
+        return null;
+    }
 }
